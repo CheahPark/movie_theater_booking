2158a7e376feb8bdeedb41036a19144224df2e48
CPark <companymoikka@gmail.com>
2021-12-18 13:39:48 +0200

    finalized the script

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const availableSeats = document.querySelectorAll('.row .seat:not(.occupied)');
 // let selectedSeats = document.querySelectorAll('.row .seat.selected');
-const count = document.getElementById('count');
+
 const movie = document.getElementById('movie');
 const total = document.getElementById('total');
+const count = document.getElementById('count');
 
 const pricePerSeat = movie.value;
 const availableSeatsArr = [...availableSeats];
@@ -11,19 +12,36 @@ const availableSeatsArr = [...availableSeats];
 let seatCounter = 0;
 let totalPrice;
 
+const displayCalculator = () => {
+  const counterValue = localStorage.getItem('seatCounter');
+  let priceValue = movie.options[movie.selectedIndex].value;
+
+  priceValue = Number(priceValue);
+
+  if (counterValue !== null || counterValue > 0) {
+    count.innerHTML = counterValue;
+    total.innerHTML = counterValue * priceValue;
+  } else {
+    count.innerHTML = 0;
+    total.innerHTML = 0;
+  }
+};
+
 const populateUI = () => {
   const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+  const counterValue = localStorage.getItem('seatCounter');
 
   if (selectedSeats !== null && selectedSeats.length > 0) {
     availableSeats.forEach((seat, index) => {
       if (selectedSeats.indexOf(index) > -1) {
         seat.classList.add('selected');
+        displayCalculator();
+        // count.innerHTML = counterValue;
+        // total.innerHTML = counterValue * total.value;
       }
     });
   }
 };
-
-populateUI();
 
 const markSeat = (selectedSeat) => {
   let selectedSeats = document.querySelectorAll('.row .seat.selected');
@@ -31,14 +49,12 @@ const markSeat = (selectedSeat) => {
 
   if (selectedSeat.classList.contains('selected')) {
     selectedSeat.classList.remove('selected');
-    seatCounter -= 1;
+    // seatCounter -= 1; // edit
     localStorage.removeItem('selectedSeats');
   } else {
     selectedSeat.classList.add('selected');
-    seatCounter += 1;
+    // seatCounter += 1; // edit
   }
-
-  localStorage.setItem('seatCounter', seatCounter);
 };
 
 const updateLocalStorage = () => {
@@ -51,47 +67,22 @@ const updateLocalStorage = () => {
 
   localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
   localStorage.setItem('seatCounter', seatsIndex.length);
-
-  console.log(seatsIndex);
 };
 
-const displaySeatCounter = () => {
-  if (
-    localStorage.getItem('seatCounter') !== null ||
-    localStoarge.getItem('seatCounter') > 0
-  ) {
-    count.innerHTML = localStorage.getItem('seatCounter');
-  } else {
-    count.innerHTML = 0;
-  }
-};
-
-const displayPrice = () => {
-  const seatCounter = localStorage.getItem('seatCounter');
-  const totalPrice = pricePerSeat * seatCounter;
-  // total.innerHTML = totalPrice;
-
-  if (
-    localStorage.getItem('seatCounter') !== null ||
-    localStoarge.getItem('seatCounter') > 0
-  ) {
-    total.innerHTML = localStorage.getItem('seatCounter') * seatCounter;
-  } else {
-    total.innerHTML = 0;
-  }
-};
+populateUI();
 
 // When a seat is clicked, change the color and display the counter
 availableSeatsArr.forEach((seat) => {
   seat.addEventListener('click', (e) => {
     const selectedSeat = e.target;
     markSeat(selectedSeat);
-    displaySeatCounter();
-    displayPrice();
+    updateLocalStorage();
+    displayCalculator();
   });
 });
 
 // When a movie is changed, calculate the total price and display it.
-movie.addEventListener('change', (e) => {
+movie.addEventListener('change', () => {
   updateLocalStorage();
+  displayCalculator();
 });
